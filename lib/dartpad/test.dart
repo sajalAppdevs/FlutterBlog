b3282a096659e4ca79e729110bb840fcb3c83517
harishankar <harishankar@zeddigital.net>
2023-05-06 12:34:01 +0530

    project setup - dartpad

---
+++ lib/dartpad/test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import '../basic_widget_text.dart';
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BasicWidgetText(),
+    ),
+  );
+}
